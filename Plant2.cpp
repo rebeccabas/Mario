d67677ea76b23218b4d0ed5ed0bdc3d7c6901860
Rubika Bashyal <bashyalrubika@gmail.com>
2023-08-14 18:41:41 +0545

    create and add features for plant 2

---
+++ Plant2.cpp
@@ -0,0 +1,37 @@
+#include "Plant2.h"
+
+
+
+Plant2::Plant2()
+{
+	file = "plant2.png";
+	try
+	{
+		if (!texture.loadFromFile("assets/" + file))/*, sf::IntRect(0, 0, Height, Width)))*/
+		{
+			throw - 1;
+		}
+	}
+	catch (int)
+	{
+		std::cout << "Error: Cannot load plant texture.";
+		exit(1);
+	}
+
+	sprite.setTexture(texture);
+
+
+	Height = 64;
+	Width = 64;
+	Velocity = 0.0;
+	velocity = { Velocity ,Velocity };
+	sprite.setOrigin(Width / 2.f, Height / 2.f);
+
+	isFriendly = false;
+	killable = false;
+}
+
+
+Plant2::~Plant2()
+{
+}
