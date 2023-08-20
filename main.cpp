79435f85a63ce4fb6699f7a2ff3f38da0fb7a2d8
Rubika Bashyal <bashyalrubika@gmail.com>
2023-08-20 21:54:12 +0545

    update background sound loop

--- main.cpp
+++ main.cpp
@@ -1,24 +1,30 @@
-#include<SFML/Graphics.hpp>
-#include<SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 #include <iostream>
 #include "Game.h"
 
 int main()
 {
-	Game game;
-	sf::SoundBuffer buffer;
-	sf::Sound bgsound;
+    Game game;
+    sf::SoundBuffer buffer;
+    sf::Sound bgsound;
 
-	if (!buffer.loadFromFile("assets/bgm.wav")) {
-		//error handling
-		std::cout << "can't find sound file" << std::endl;
-	}
-	bgsound.setBuffer(buffer);
+    if (!buffer.loadFromFile("assets/bgm.wav")) {
+        // error handling
+        std::cout << "Can't find sound file" << std::endl;
+    }
+    bgsound.setBuffer(buffer);
 
-	if (bgsound.getStatus() != sf::Sound::Playing) {
-		bgsound.play();
+    if (bgsound.getStatus() != sf::Sound::Playing) {
+        bgsound.setLoop(true);  // Set the sound to loop
+        bgsound.play();
+    }
 
-	}
-	game.run();
-	return 0;
+    game.run();
+    return 0;
 }
+
+
+
+
+
